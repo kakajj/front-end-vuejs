9484721423eeb8591173c83df3345308ddaac749
kakajj <56553177+kakajj@users.noreply.github.com>
2021-04-21 22:20:24 +0700

    UPDATE first basic

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>เจอกันอีกแล้วไอพวกเหี้ย</h1>
+    <img alt="Vue logo" src="../assets/ped.png">
   </div>
 </template>
+<script>
+
+export default {
+  name: 'About',
+}
+</script>
+
+<style scoped>
+img {
+  @apply max-w-sm mx-auto
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>สวัสดีไอพวกเหี้ย</h1>
+    <img alt="Vue logo" src="../assets/ped.png">
+    
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
+
+<style scoped>
+img {
+  @apply max-w-sm mx-auto
+}
+</style>
