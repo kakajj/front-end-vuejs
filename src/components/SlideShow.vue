8bfa723f61fd9ee02adef668b450ceb11dd0f8e6
UntillNIGHT <lengleng9090@gmail.com>
2021-04-29 20:03:14 +0700

    [UPDATE] Slide Show

--- src/components/SlideShow.vue
+++ src/components/SlideShow.vue
@@ -1,35 +1,89 @@
 <template>
-  <div class="text-center">This is SlideShow</div>
+  <div>
+    <transition-group name="fade" tag="div">
+      <div v-for="i in [currentIndex]" :key="i">
+        <img :src="currentImg" />
+      </div>
+    </transition-group>
+    <a class="prev mx-10" @click="prev" href="#">&#10094;</a>
+    <a class="next mx-10" @click="next" href="#">&#10095;</a>
+  </div>
 </template>
 
 <script>
 
 export default {
-  components: { },
   data() {
     return {
-      slides: [
-        {
-          title: "Samsung Galaxy S20",
-          content: "New Ultimate Smartphone",
-          image: require("@/assets/s20.jpg"),
-        },
-        {
-          title: "Oppo F17 Pro",
-          content: "New Ultimate Smartphone",
-          image: require("@/assets/oppo.jpg"),
-        },
-        {
-          title: "Iphone 12 ",
-          content: "Best Smartphone in the world",
-          image: require("@/assets/iphone.png"),
-        },
+      images: [
+        require("../assets/s20.jpg"),
+        require("../assets/ped.png"),
+        "https://cdn.pixabay.com/photo/2015/05/15/14/27/eiffel-tower-768501_1280.jpg",
+        "https://cdn.pixabay.com/photo/2016/12/04/19/30/berlin-cathedral-1882397_1280.jpg"
       ],
+      timer: null,
+      currentIndex: 0
     };
   },
+
+  mounted: function() {
+    this.startSlide();
+  },
+
+  methods: {
+    startSlide: function() {
+      this.timer = setInterval(this.next, 4000);
+    },
+
+    next: function() {
+      this.currentIndex += 1;
+    },
+    prev: function() {
+      this.currentIndex -= 1;
+    }
+  },
+
+  computed: {
+    currentImg: function() {
+      return this.images[Math.abs(this.currentIndex) % this.images.length];
+    }
+  }
 };
 </script>
 
 <style scoped>
 
+img {
+  height:600px;
+  width:100%
+}
+
+.prev, .next {
+  cursor: pointer;
+  position: absolute;
+  top: 40%;
+  width: auto;
+  padding: 16px;
+  color: white;
+  font-weight: bold;
+  font-size: 18px;
+  transition: 0.7s ease;
+  border-radius: 0 4px 4px 0;
+  text-decoration: none;
+  user-select: none;
+}
+
+.next {
+  position:absolute;
+  right: 4%;
+
+}
+/* 
+.prev {
+  left: 0;
+} */
+
+.prev:hover, .next:hover {
+  background-color: rgba(0,0,0,0.9);
+}
 </style>
